0b0928016a9b130b983e66bf0a82c5b75a6a1566
Caden01 <caden.carlson01@gmail.com>
2023-08-16 12:06:08 -0700

    created a function that return an object with the type of species and type of noise it makes

--- script.js
+++ script.js
@@ -48,3 +48,21 @@ const instr = {
     return this.firstName + " says bye!";
   },
 };
+
+// createAnimal function
+const d = createAnimal("dog", "bark", "Woooof!");
+// {species: "dog", bark: ƒ}
+d.bark(); //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa");
+// {species: "sheep", bleet: ƒ}
+s.bleet(); //"BAAAAaaaa"
+
+function createAnimal(species, verb, noise) {
+  return {
+    species,
+    [verb]() {
+      return noise;
+    },
+  };
+}
